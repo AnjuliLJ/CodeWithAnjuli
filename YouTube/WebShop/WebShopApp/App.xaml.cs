527bd5d06646f338c4ca35e683727ecfda3dc585
AnjuliLJ <anjuli.jhakry@outlook.com>
2024-02-15 16:37:41 +0100

    Created .NET MAUI app

---
+++ YouTube/WebShop/WebShopApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WebShopApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ YouTube/WebShop/WebShopApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WebShopApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
