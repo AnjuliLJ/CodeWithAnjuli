baef5442ddae00fc70857b955504bb49a5cbd85f
AnjuliLJ <anjuli.jhakry@outlook.com>
2024-07-25 15:31:03 +0200

    playing around with .net maui

---
+++ Misc/MAUI_PSW/PassXYZ.Vault/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Diagnostics;
+
+namespace PassXYZ.Vault;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+
+	protected override void OnStart()
+	{
+		Debug.WriteLine("PassXYZ.Vault.XYZ: OnStart");
+		base.OnStart();
+	}
+
+	protected override void OnSleep()
+	{
+		Debug.WriteLine("PassXYZ.Vault.XYZ: OnSleep");
+		base.OnSleep();
+	}
+
+	protected override void OnResume()
+	{
+		Debug.WriteLine("PassXYZ.Vault.XYZ: OnResume");
+		base.OnResume();
+	}
+}
---
+++ Misc/MAUI_PSW/PassXYZ.Vault/PxWindow.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace PassXYZ.Vault;
+
+public class PxWindow : Window
+{
+    public PxWindow() : base() {}
+
+    public PxWindow(Page page) : base(page) {}
+
+    protected override void OnCreated() {
+        Debug.WriteLine("PassXYZ.Vault.App: 1. OnCreated");
+    }
+
+    protected override void OnActivated() {
+        Debug.WriteLine("PassXYZ.Vault.App: 2. OnActivated");
+    }
+
+        protected override void OnDeactivated() {
+        Debug.WriteLine("PassXYZ.Vault.App: 3. OnDeactivated");
+    }
+
+        protected override void OnStopped() {
+        Debug.WriteLine("PassXYZ.Vault.App: 4. OnStopped");
+    }
+        protected override void OnResumed() {
+        Debug.WriteLine("PassXYZ.Vault.App: 5. OnResumed");
+    }
+    
+        protected override void OnDestroying() {
+        Debug.WriteLine("PassXYZ.Vault.App: 6. OnDestroying");
+    }
+}
